8f26396ac990081a1973d1c68913c0544df025a8
beste <1596775839@qq.com>
2024-06-02 10:19:50 +0800

    FEAT:Support network function

--- board/myir/myd-lma35/myd-lma35.c
+++ board/myir/myd-lma35/myd-lma35.c
@@ -7,11 +7,35 @@
 #include <dm.h>
 #include <log.h>
 #include <wdt.h>
+#include <linux/bitops.h>
+#include <asm-generic/gpio.h>
+#include <linux/delay.h>
 
 DECLARE_GLOBAL_DATA_PTR;
 
 int board_late_init(void)
 {
+	unsigned int gpio_net0=94,gpio_net1=181;
+
+	gpio_request(gpio_net0, "gmac0_rst");
+	gpio_request(gpio_net1, "gmac1_rst");
+
+	gpio_direction_output(gpio_net0, 0);
+	gpio_direction_output(gpio_net1, 0);
+	
+
+	gpio_set_value(gpio_net0, 1);
+	gpio_set_value(gpio_net1, 1);
+	mdelay(10);
+	gpio_set_value(gpio_net0, 0);
+	gpio_set_value(gpio_net1, 0);
+	mdelay(10);
+	gpio_set_value(gpio_net0, 1);
+	gpio_set_value(gpio_net1, 1);
+	mdelay(20);
+
+	gpio_free(gpio_net0);
+	gpio_free(gpio_net1);
 	return 0;
 }
 
